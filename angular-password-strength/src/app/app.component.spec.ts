78053a7106b1d04e929d167e21841b3e48ba2ba4
humblezxc <you@example.com>
2023-06-13 15:58:06 +0200

    Fix node_modules problem and replace app.component.spec.ts to Chai-s syntax

--- angular-password-strength/src/app/app.component.spec.ts
+++ angular-password-strength/src/app/app.component.spec.ts
@@ -1,29 +1,34 @@
+/// <reference lib="dom" />
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { expect } from 'chai';
+// Change to Chai's syntax
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
+    });
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).to.be.ok;
   });
 
-  it(`should have as title 'angular_password_strength'`, () => {
+  it(`should have as title 'angular-password-strength'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-password-strength');
+    expect(app.title).to.equal('angular-password-strength');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-password-strength app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).to.contain('angular-password-strength app is running!');
   });
 });
